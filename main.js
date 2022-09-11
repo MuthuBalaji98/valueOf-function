83aeedb918ffb26c5dd2644ad16e79dff874f757
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 21:26:24 +0530

    valueOf

---
+++ main.js
@@ -0,0 +1,7 @@
+let subject=["Tamil","English","Maths","Science","Social"];
+console.log("original array :",subject);
+console.log("length of the array is "+subject.length);
+console.log("method : array.valueOf()");
+let result=subject.valueOf();
+console.log("change in array : ", result);
+console.log("return: length of array after valueOf() is "+result.length);
